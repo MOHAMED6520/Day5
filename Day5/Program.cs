22d1eeffaf79f2c672ef730ab28ab528c82e2ec1
moham <moham@DESKTOP-M8CT1H1>
2025-07-15 09:27:19 +0300

    Boxing And UnBoxing

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.Boxing And UnBoxing
+            //Boxing And UnBoxing
+
+            //object o1;
+            //o1=2;                 //Cast From ValueType [int] To ReferenceType [object]      Boxing
+            //o1 = 1.5;             //Cast From ValueType [double] To ReferenceType [object]   Boxing
+            //o1 = 'm';             //Cast From ValueType [char] To ReferenceType [object]     Boxing
+            //o1 = true;            //Cast From ValueType [bool] To ReferenceType [object]     Boxing
+            //o1 = new DateTime();  //Cast From ValueType [DateTime] To ReferenceType [object] Boxing
+
+
+            //int x = 5;
+            //object o1 = x;
+
+            //// UnBoxing
+            //object o1 = 5;
+            //int x =(int) o1; 
+            #endregion
+
+
         }
     }
 }
