bd9a370d56d16ef73c9980514e44bf6eb9e08829
moham <moham@DESKTOP-M8CT1H1>
2025-07-16 07:52:00 +0300

    Q01

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        #region Q01
+        //enum WeekDays
+        //{
+
+        //    Monday,
+        //    Tuesday,
+        //    Wednesday,
+        //    Thursday,
+        //    Friday,
+        //    Saturday,
+        //    Sunday
+
+
+        //} 
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //string[] g = {"0" ,"1" , "2" , "3", "4", "5", "6" };
+            //WeekDays weekDays01 = WeekDays.Monday;
+
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    weekDays01 =(WeekDays) Enum.Parse(typeof(WeekDays), g[i]);
+            //    Console.WriteLine(weekDays01);
+            //} 
+            #endregion
         }
     }
 }
